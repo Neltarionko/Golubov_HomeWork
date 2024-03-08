9b8bd3e3e151465ecef52d59857c70660f42f618
Fedor Baryshev <fedbar02@gmail.com>
2024-03-08 18:55:09 +0300

    add rrt_connect + rrt

--- main.py
+++ main.py
@@ -1,4 +1,47 @@
 import numpy as np 
-
+import matplotlib.pyplot as plt
+from rrt import *
+from rrt_connect import *
+'''
 map = np.loadtxt("map.txt", dtype=float)
-print(map)
+map = np.rot90(map)
+map = map[70:240,25:240]
+np.savetxt("map2.txt", map, fmt="%d")
+plt.imshow(map, cmap='Greys')
+plt.show()
+'''
+
+rrt_mod = 1
+
+if rrt_mod == 0:
+    start = (150,50)
+    end = (20,150)
+    end_area = 2
+    growth_factor = 20
+    map = np.loadtxt("map2.txt", dtype=float)
+    rrt = RRT(start,end,map,end_area,growth_factor)
+    path = rrt.make_tree(100000)
+    plt.imshow(map, cmap='Greys')
+    rrt.print_all_tree()
+    plt.plot(start[1],start[0],"bo")
+    plt.plot(end[1],end[0],"go")
+    plt.plot(path[0:path.shape[0],1],path[0:path.shape[0],0],"b",ms = 3)
+    plt.show()
+elif rrt_mod == 1:
+    start = (150,50)
+    end = (20,150)
+    end_area = 30
+    growth_factor = 20
+    map = np.loadtxt("map2.txt", dtype=float)
+    rrt1 = RRT_connect(start,end,map,end_area,growth_factor)
+    rrt2 = RRT_connect(end,start,map,end_area,growth_factor)
+    rrt1.get_tree(rrt2)
+    rrt2.get_tree(rrt1)
+    path = rrt1.make_tree()
+    plt.imshow(map, cmap='Greys')
+    rrt1.print_all_tree()
+    rrt2.print_all_tree()
+    plt.plot(start[1],start[0],"bo")
+    plt.plot(end[1],end[0],"go")
+    plt.plot(path[0:path.shape[0],1],path[0:path.shape[0],0],color="#7cb3e3b0")
+    plt.show()
