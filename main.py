317fbef990e370798b97df7cd61792e9807630cd
Neltarion <vin-alyosha@mail.ru>
2024-03-14 00:09:54 +0300

    Commented

--- main.py
+++ main.py
@@ -7,17 +7,22 @@ class Graph():
     def __init__(self,map) -> None:
         self.map = map
         self.G = nx.Graph()
-        self.all_poit = []
+        self.all_point = []
     
     def make_grapf(self,point = [(1,1),(2,2)]):
-        if point in self.all_poit:
-            return
+        '''Рекурсивно добавляет связи между графом и передаваемой вершиной 
+
+        point: Массив из координат двух роботов
+        return: Заполненный граф
+        '''
+        if point in self.all_point:
+            return None
         robot1 = point[0]
         robot2 = point[1]
-        self.all_poit.append(point)
+        self.all_point.append(point)
         for y1 in range(max(0,robot1[0]-1),min(self.map.shape[0],robot1[0]+2)):
             for x1 in range(max(0,robot1[1]-1),min(self.map.shape[1],robot1[1]+2)):
-                if abs(y1-robot1[0]) + abs(x1-robot1[1]) == 2:
+                if abs(y1-robot1[0]) + abs(x1-robot1[1]) == 2:  # Проверка на диагональ
                     continue
                 for y2 in range(max(0,robot2[0]-1),min(self.map.shape[0],robot2[0]+2)):
                     for x2 in range(max(0,robot2[1]-1),min(self.map.shape[1],robot2[1]+2)):
@@ -31,10 +36,14 @@ class Graph():
                             self.make_grapf([(y1,x1),(y2,x2)])
         return self.G
 
-def plot_one_point(G,poit):
+def plot_one_point(G,point):
+    '''Отрисовка вершины и всех ее связей
+
+    point: Точка для отрисовки
+    '''
     G2 = nx.Graph()
-    for n in G.neighbors(poit):
-        G2.add_edge(poit,n)
+    for n in G.neighbors(point):
+        G2.add_edge(point,n)
     nx.draw_spring(G2, with_labels = True)
     plt.show()
     
@@ -57,15 +66,14 @@ def make_map():
     return map
 
 map = make_map()
-#plt.imshow(map, cmap='Greys')
-#plt.show()
+
 sys.setrecursionlimit(100000)
 
 GR = Graph(map)
-G = GR.make_grapf()
+end_graph = GR.make_grapf()
 mod = 1
 point = str([(1,2),(2,2)])
 if mod == 0:
-    plot_one_point(G,point)
+    plot_one_point(end_graph,point)
 else:
-    plot_all_grpah(G)
+    plot_all_grpah(end_graph)
